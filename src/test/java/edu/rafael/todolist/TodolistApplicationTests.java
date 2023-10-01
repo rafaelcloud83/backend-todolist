701065639de41287c46b7e7d3e723b2c0f9b3882
rafaelcloud83 <rafaelcloud83@gmail.com>
2023-10-01 18:10:20 -0300

    primeiro commit

---
+++ src/test/java/edu/rafael/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rafael.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
