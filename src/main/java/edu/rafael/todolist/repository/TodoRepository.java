086fb157c576dc557b3c0b068bb7becf3d9723a5
rafaelcloud83 <rafaelcloud83@gmail.com>
2023-10-01 18:31:53 -0300

    feat: criado pacote repository e classe TodoRepository

---
+++ src/main/java/edu/rafael/todolist/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package edu.rafael.todolist.repository;
+
+import edu.rafael.todolist.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
